fc187604584a6bdf618474ddef4fe1a8f7f7e31b
Ben Shutt <ben@3sidedcube.com>
2021-10-12 12:58:28 +0100

    Add swizzled observer extension to `NotificationCenter`

---
+++ Swizzle/NotificationCenter+Swizzle.swift
@@ -0,0 +1,39 @@
+//
+//  NotificationCenter+Swizzle.swift
+//  Swizzle
+//
+//  Created by Ben Shutt on 12/10/2021.
+//  Copyright © 2020 3 SIDED CUBE APP PRODUCTIONS LTD. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension NotificationCenter {
+
+    /// Add Swizzled observer
+    ///
+    /// - Parameters:
+    ///   - name: `NSNotification.Name`
+    ///   - object: `Any`
+    ///   - queue: `OperationQueue`
+    ///   - block: Closure
+    ///
+    /// - Returns: `NSObjectProtocol`
+    func addSwizzledObserver(
+        forName name: NSNotification.Name,
+        object: Any? = nil,
+        queue: OperationQueue = .main,
+        using block: @escaping (UIViewController) -> Void
+    ) -> NSObjectProtocol {
+        return addObserver(
+            forName: name,
+            object: object,
+            queue: queue
+        ) { sender in
+            let key = kViewControllerNotificationKey
+            guard let viewController = sender.userInfo?[key] as? UIViewController else { return }
+            block(viewController)
+        }
+    }
+}
--- Swizzle/Swizzle.h
+++ Swizzle/Swizzle.h
@@ -3,6 +3,7 @@
 //  Swizzle
 //
 //  Created by Ben Shutt on 12/10/2021.
+//  Copyright © 2020 3 SIDED CUBE APP PRODUCTIONS LTD. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -15,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char SwizzleVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Swizzle/PublicHeader.h>
 
-
+#import <Swizzle/UIViewController+Swizzle.h>
